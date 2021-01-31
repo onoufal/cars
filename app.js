ed003dd0e06270a1cdbb67aa82ab00fa263ab57b
onoufal <osama.h.noufal@gmail.com>
2021-01-31 19:44:47 +0200

    adding lab05

--- app.js
+++ app.js
@@ -18,4 +18,18 @@ if (localTime <= 12 && localTime >=6 ) {
 
 document.write('This web page presented for ' +firstName + " " + familyName)
 
+// LAB 05
+var car = '<img src="car.jpg alt="">'
+var numCars = 0
+
+while (numCars <3 || numCars > 9) {
+    numCars = prompt("How many cars do you want? choose between 3 and 9")
+}
+
+for (var i = 0; i <= numCars; i++) {
+    document.write(car);
+}
+
+
+
 
