aa5ae4a7906d6d896b85caaed51bddf1d223a746
onoufal <osama.h.noufal@gmail.com>
2021-02-01 11:02:43 +0200

    submitting lab 06 functions

--- app.js
+++ app.js
@@ -2,33 +2,45 @@ var firstName = prompt('What is your 1st name?')
 var familyName = prompt('what is your family name?')
 alert('welcome ' + firstName + familyName)
 
+function gettime() {
 
-var localTime = prompt('What is your current time?')
-console.log(localTime)
-localTime = Number(localTime)
-console.log(localTime)
-if (localTime <= 12 && localTime >=6 ) {
-    alert('Good morning ' + firstName)
-} else if (localTime <= 20 && localTime >=13) {
-    alert('Good evning ' + firstName)
-} else {
-    alert("Go sleep " + firstName + ' !') 
 
+    var localTime = prompt('What is your current time?')
+    console.log(localTime)
+    localTime = Number(localTime)
+    console.log(localTime)
+    if (localTime <= 12 && localTime >= 6) {
+        alert('Good morning ' + firstName)
+    } else if (localTime <= 20 && localTime >= 13) {
+        alert('Good evning ' + firstName)
+    } else {
+        alert("Go sleep " + firstName + ' !')
+
+    }
+
+
+    document.write('This web page presented for ' + firstName + " " + familyName)
 }
 
-document.write('This web page presented for ' +firstName + " " + familyName)
+gettime()
+
 
 // LAB 05
-var car = '<img src="car.jpg alt="">'
-var numCars = 0
 
-while (numCars <3 || numCars > 9) {
+var car = '<img src="car.jpg" alt="">'
+// var numCars = 0
+
+var numCars = prompt("How many cars do you want?")
+
+while (numCars < 3 || numCars > 9) {
     numCars = prompt("How many cars do you want? choose between 3 and 9")
 }
 
-for (var i = 0; i <= numCars; i++) {
+for (var i = 0; i < numCars; i++) {
     document.write(car);
 }
+
+
 
 
 
